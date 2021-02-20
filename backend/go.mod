14ae7c1b16a266af1580bb7bf022926903eac35d
phpunch <nutacp_ryho@hotmail.com>
2021-02-20 11:16:37 +0700

    Implement sse function

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/phpunch/interview
 
 go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var messageChan chan Message
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handleSSE(w, r)
+	case "POST":
+		sendMessage(w, r)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func handleSSE(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("Get handshake from client")
+
+	// prepare the header
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// instantiate the channel
+	messageChan = make(chan Message)
+
+	// close the channel after exit the function
+	defer func() {
+		close(messageChan)
+		messageChan = nil
+		log.Printf("client connection is closed")
+	}()
+
+	// prepare the flusher
+	flusher, _ := w.(http.Flusher)
+
+	// trap the request under loop forever
+	for {
+
+		select {
+
+		// message will received here and printed
+		case message := <-messageChan:
+			fmt.Fprintf(w, "%+v\n", message)
+			flusher.Flush()
+
+		// connection is closed then defer will be executed
+		case <-r.Context().Done():
+			return
+
+		}
+	}
+}
+
+type Message struct {
+	User    string `json:"user"`
+	Message string `json:"message"`
+}
+
+func sendMessage(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("body error")
+	}
+
+	var m Message
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		fmt.Printf("unmarshal error")
+	}
+
+	if messageChan != nil {
+		log.Printf("print message to client")
+
+		// send the message through the available channel
+		messageChan <- m
+	}
+}
 
 func main() {
-	fmt.Println("HELLO")
+
+	http.HandleFunc("/message", handler)
+
+	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:5000", nil))
 }
