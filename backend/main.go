33ea15f8d489833c757a83c125fb7e54bc6072b7
phpunch <nutacp_ryho@hotmail.com>
2021-02-20 11:54:34 +0700

    Fix handler

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
